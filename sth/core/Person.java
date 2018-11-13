133e673688c6719a12463441ab53c76fc12f6dca
Tomas Lopes <joaotomaslopes@hotmail.com>
2018-11-13 11:46:19 +0000

    Missing tabs

--- sth/core/Person.java
+++ sth/core/Person.java
@@ -6,45 +6,45 @@ import sth.core.exception.BadEntryException;
 
 public abstract class Person implements Serializable, Comparable<Person> {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private Integer _id;
-    private String _name;
-    private int _phoneNumber;
+	private String _name;
+	private int _phoneNumber;
 
-    Person(Integer id, int phoneNumber, String name) {
-        _name = name;
-        _id = id;
-        _phoneNumber = phoneNumber;
-    }  
+	Person(Integer id, int phoneNumber, String name) {
+		_name = name;
+		_id = id;
+		_phoneNumber = phoneNumber;
+	}
 
-    void setPhoneNumber(int phoneNumber) {
-    	_phoneNumber = phoneNumber;
-    }
-    
-    public String getName() {
-        return _name;
-    }
+	void setPhoneNumber(int phoneNumber) {
+		_phoneNumber = phoneNumber;
+	}
+	
+	public String getName() {
+		return _name;
+	}
 
-    public Integer getId() {
-        return _id;
-    }
+	public Integer getId() {
+		return _id;
+	}
 
-    protected abstract String getLabel();
+	protected abstract String getLabel();
 
-    @Override
-    public int compareTo(Person p) {
-    	return getId() - p.getId();
-    }
-    
-    public String toString() {
-        return String.format("%s|%d|%d|%s", getLabel(), _id, _phoneNumber, _name);
-    }
+	@Override
+	public int compareTo(Person p) {
+		return getId() - p.getId();
+	}
+	
+	public String toString() {
+		return String.format("%s|%d|%d|%s", getLabel(), _id, _phoneNumber, _name);
+	}
 
 	void parseContext(String context, School school) throws BadEntryException {
-    	throw new BadEntryException("Should not have extra context: " + context);
+		throw new BadEntryException("Should not have extra context: " + context);
 	}
 
 }
--- sth/core/Student.java
+++ sth/core/Student.java
@@ -7,55 +7,55 @@ import sth.core.exception.BadEntryException;
 
 public class Student extends Person {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private Set<Discipline> _disciplines = new TreeSet<Discipline>();
 	private boolean _isRepresentative = false;
-    private Course _course;
+	private Course _course;
 
-    Student(int id, int phoneNumber, String name, boolean isRepresentative) {
-        super(id, phoneNumber, name);
-        _isRepresentative = isRepresentative;
-    }
+	Student(int id, int phoneNumber, String name, boolean isRepresentative) {
+		super(id, phoneNumber, name);
+		_isRepresentative = isRepresentative;
+	}
 
-    void setRepresentative(boolean isRepresentative) {
-        _isRepresentative = isRepresentative;
-    }
-    
-    boolean isRepresentative() {
-        return _isRepresentative;
-    }
+	void setRepresentative(boolean isRepresentative) {
+		_isRepresentative = isRepresentative;
+	}
+	
+	boolean isRepresentative() {
+		return _isRepresentative;
+	}
 
-    @Override
-    void parseContext(String lineContext, School school) throws BadEntryException {
-        String components[] =  lineContext.split("\\|");
+	@Override
+	void parseContext(String lineContext, School school) throws BadEntryException {
+		String components[] =  lineContext.split("\\|");
 
-        if (components.length != 2)
-            throw new BadEntryException("Invalid line context " + lineContext);
+		if (components.length != 2)
+			throw new BadEntryException("Invalid line context " + lineContext);
 
-        if (_course == null) {
-            _course = school.parseCourse(components[0]);
-            _course.addStudent(this);
-        }
+		if (_course == null) {
+			_course = school.parseCourse(components[0]);
+			_course.addStudent(this);
+		}
 
-        Discipline dis = _course.parseDiscipline(components[1]);
-        dis.enrollStudent(this);
-        _disciplines.add(dis);
-    }
+		Discipline dis = _course.parseDiscipline(components[1]);
+		dis.enrollStudent(this);
+		_disciplines.add(dis);
+	}
 
-    @Override
-    protected String getLabel() {
-        return _isRepresentative ? "DELEGADO" : "ALUNO";
-    }
+	@Override
+	protected String getLabel() {
+		return _isRepresentative ? "DELEGADO" : "ALUNO";
+	}
 
-    @Override
-    public String toString() {
-		  String header = super.toString();
-		  for (Discipline d : _disciplines)
-			  header += '\n' + d.show();
-		  return header;
-    }
-    
+	@Override
+	public String toString() {
+		String header = super.toString();
+		for (Discipline d : _disciplines)
+			header += '\n' + d.show();
+		return header;
+	}
+	
 }
